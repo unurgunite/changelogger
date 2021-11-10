5a9f959ebd2f1e0985579d0838b7d7e51ef0ee83
unurgunite <senpaiguru1488@gmail.com>
2021-11-10 18:35:28 +0300

    Add branches window

--- lib/changelogger/branches_window.rb
+++ lib/changelogger/branches_window.rb
@@ -1,22 +1,67 @@
-module Changelogger
-  class BranchWindow
-    def initialize
-      branches
-    end
+# frozen_string_literal: true
 
-    def branches
-       #branches_win = Curses::Window.new(Curses.lines / 2 - 1, Curses.cols / 2 - 1, 0, 0)
-      win2 = Curses::Window.new(Curses.lines / 2 - 1, Curses.cols / 2 - 1,
-                            Curses.lines / 2, Curses.cols / 2)
-      win2.box("|", "-")
-      win2.refresh
-      2.upto(win2.maxx - 3) do |i|
-        win2.setpos(win2.maxy / 2, i)
-        win2 << "*"
-        win2.refresh
-        sleep 0.05
+module Changelogger
+  # +Changelogger::Graph+ class represents an interface of graph object, which renders in a separate window
+  class Graph
+    class << self
+      # Name of the file, where to store graphs
+      FILENAME = ".graph"
+
+      # +Changelogger::Graph.width+                 -> Integer
+      #
+      # +Changelogger::Graph.width+ returns the size of the biggest line in a file to then set this value as
+      # default window object width attribute
+      # @return [Integer]
+      def width
+        File.open(FILENAME, "w") { |file| file.write(`git log --graph`) }
+        IO.foreach(FILENAME).max_by(&:length).size
+      end
+
+      # +Changelogger::Graph.build+                 -> String
+      #
+      # +Changelogger::Graph.build+ field builds graph object in a separate window
+      # @return [String]
+      def build
+        "No such file: #{Dir.pwd}/#{FILENAME}" unless File.file?(FILENAME)
+        File.read(".graph")
       end
     end
   end
-end
 
+  # +Changelogger::BranchWindow+ class represents an interface of a graph's window
+  class BranchWindow
+    # +Changelogger::BranchWindow.new+            -> obj
+    #
+    # @param [Integer] height Value of window height
+    # @param [Integer] width Value of window width
+    # @param [Integer] top Value of window position relative to the top of the last
+    # @param [Integer] left Value of window position relative to the left of the last
+    # @return [object]
+    def initialize(height: 50, width: Graph.width + 20, top: 10, left: 0)
+      @height = height
+      @width = width
+      @top = top
+      @left = left
+      branches
+    end
+
+    private
+
+    # +Changelogger::BranchWindow#branches+       -> value
+    #
+    # +Changelogger::BranchWindow#branches+ field is used to prepare window to show graph
+    # @private
+    # @return [Object]
+    def branches
+      win = Curses::Window.new(@height, @width, @top, @left)
+      win.box
+      # win.scrollok true
+      win.refresh
+      win1 = win.subwin(@height - 2, @width - 2, @top, @left + 1)
+      win1.setpos(1, 1)
+      win1.addstr(Graph.build)
+      # win1.refresh
+      win1.getch
+    end
+  end
+end
--- lib/changelogger/main.rb
+++ lib/changelogger/main.rb
@@ -4,11 +4,9 @@ require "curses"
 require_relative "header"
 require_relative "branches_window"
 
-
 Curses.init_screen
 Curses.curs_set(0)
 
 Changelogger::Header.new
 
 Changelogger::BranchWindow.new
-
