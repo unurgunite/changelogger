84cefbd5f4ff10211ded79b4d0b8dce01bb4d937
unurgunite <senpaiguru1488@gmail.com>
2021-11-10 18:37:17 +0300

    Fix docs for header file

--- lib/changelogger/header.rb
+++ lib/changelogger/header.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-module Changelogger # :nodoc:
+module Changelogger
   # +Changelogger::Header+ class is an interface for header of TUI
   class Header
-    # +Changelogger::Header.new+                    -> object
+    # +Changelogger::Header.new+                    -> value
     #
     # @param [Integer] height Value of window height
     # @param [Integer] width Value of window width
     # @param [Integer] top Value of window position relative to the top of the last
     # @param [Integer] left Value of window position relative to the left of the last
-    # @return [object]
+    # @return [Object]
     def initialize(height: 0, width: Curses.cols, top: 0, left: 0)
       @height = height
       @width = width
@@ -21,23 +21,23 @@ module Changelogger # :nodoc:
 
     private
 
-    # +Changelogger::Header.header_win+             -> object
+    # +Changelogger::Header.header_win+             -> value
     #
     # @private
-    # @return [object]
-    # +Changelogger::Header.header_win+ method is used to initialize header's frame
+    # @return [Object]
+    # +header_win+ method is used to initialize header's frame
     def header_win
       @header_win = Curses::Window.new(@height, @width, @top, @left)
       @header_win.box(" ", " ", " ")
     end
 
-    # +Changelogger::Header.line+                   -> object
+    # +Changelogger::Header.line+                   -> value
     #
     # @private
-    # @return [object]
-    # +Changelogger::Header.line+ method is used to draw a line with gem's name and its version
+    # @return [Object]
+    # +line+ method is used to draw a line with gem's name and its version
     def line
-      line = @header_win.subwin(0, @width, @top, @left)
+      line = @header_win.subwin(@height, @width, @top, @left)
       line.addstr(" Changelogger #{Changelogger::VERSION} ".center(@width, "="))
       line.refresh
     end
